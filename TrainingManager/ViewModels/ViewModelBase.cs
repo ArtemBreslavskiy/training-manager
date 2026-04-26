f3326d5fb549a2185c6a4bbd48e9a6b63d46478a
Charlssssss33 <archernov2010@gmail.com>
2026-04-26 13:11:33 +0500

    Добавьте файлы проекта.

---
+++ TrainingManager/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TrainingManager.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ TrainingManager/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TrainingManager.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
